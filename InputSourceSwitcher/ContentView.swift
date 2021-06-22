8d3c90cbe0f7ddcdd02d115ff2ec842aa12f6a35
yxj <564197835@qq.com>
2021-06-22 09:45:02 +0800

    Initial Commit

---
+++ InputSourceSwitcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InputSourceSwitcher
+//
+//  Created by 杨希杰 on 2021/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InputSourceSwitcher/InputSourceSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputSourceSwitcherApp.swift
+//  InputSourceSwitcher
+//
+//  Created by 杨希杰 on 2021/6/22.
+//
+
+import SwiftUI
+
+@main
+struct InputSourceSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

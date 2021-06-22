43c47b88990db99f3fd7d91d125843740dadcd17
yxj <564197835@qq.com>
2021-06-22 14:18:35 +0800

    get input sources

---
+++ InputSourceSwitcher/Struct/InputSources.swift
@@ -0,0 +1,19 @@
+//
+//  InputSources.swift
+//  InputSourceSwitcher
+//
+//  Created by 杨希杰 on 2021/6/22.
+//
+
+import Foundation
+
+struct InputSource: Identifiable {
+    var name: String = ""
+    var id: Int = 0
+}
+
+extension InputSource: CustomStringConvertible {
+    var description: String {
+        return name
+    }
+}
---
+++ InputSourceSwitcher/SwiftUI/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  InputSourceSwitcher
+//
+//  Created by 杨希杰 on 2021/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var currentInputSources: [InputSource] = []
+
+    var body: some View {
+        VStack {
+            // show switch buttons
+            ForEach(currentInputSources) { inputSource in
+                HStack {
+                    Text(inputSource.name)
+                    Button("Switch To \(inputSource.name)") {
+                        print("[User Action] Button Switch To \(inputSource.name) Clicked!")
+                        SwitchInputSource(to: inputSource.name)
+                    }
+                }
+            }
+
+            Spacer()
+
+            // get input sources from menu bar
+            Button("Reset") {
+                print("[User Action] Button Reset Clicked!")
+                currentInputSources = GetCurrentInputSourcesInMenubar()
+            }
+        }
+        .padding()
+        .frame(minWidth: 300, maxWidth: 600, minHeight: 200, maxHeight: 400)
+    }
+}
+
+// struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+// }

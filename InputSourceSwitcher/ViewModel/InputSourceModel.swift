955ff8aca3abbc9fbec2c35f990ca9660f7ba3ec
yxj <564197835@qq.com>
2021-06-24 01:37:32 +0800

    finish Update()

--- InputSourceSwitcher/ViewModel/InputSourceModel.swift
+++ InputSourceSwitcher/ViewModel/InputSourceModel.swift
@@ -44,17 +44,29 @@ class InputSourcesModel: ObservableObject {
         // Next, refresh InputSourcesModel
 
         // Firstly, find the removed Input Sources
-        let originalInputSources: Set<InputSource> = Set(model.inputSources)
-        let newInputSources: Set<InputSource> = Set(model.GetNewInputSourcesFromSystem()) // 这是个异步的吧……好像没获取完？？
+        let originalInputSources: [InputSource] = model.inputSources
+        let newInputSources: [InputSource] = model.GetNewInputSourcesFromSystem()
 
-        // Then, use their names to reset their shortcuts
-        let inputSourcesToResetShortcut: Set<InputSource> = originalInputSources.subtracting(newInputSources)
+        var original: [String] = []
+        for item in originalInputSources {
+            original.append(item.name)
+        }
+
+        var new: [String] = []
+        for item in newInputSources {
+            new.append(item.name)
+        }
+
+        let inputSourcesToResetShortcut: Set<String> = Set(original).subtracting(Set(new))
+
         print("[Model Debug]\n\toriginalInputSources:\(originalInputSources)\n\tnewInputSources:\(newInputSources)\n\tinputSourcesToResetShortcut:\(inputSourcesToResetShortcut)\n")
 
-        if inputSourcesToResetShortcut != Set<InputSource>() {
+        // Then, use their names to reset their shortcuts
+
+        if inputSourcesToResetShortcut != Set<String>() {
             for inputSource in inputSourcesToResetShortcut {
-                KeyboardShortcuts.reset(KeyboardShortcuts.Name(inputSource.name))
-                print("[KeyboardShortcuts] reset \(inputSource.name)")
+                KeyboardShortcuts.reset(KeyboardShortcuts.Name(inputSource))
+                print("[KeyboardShortcuts] reset \(inputSource)")
             }
         }
 

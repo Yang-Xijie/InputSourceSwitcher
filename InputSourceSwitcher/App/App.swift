1a97f442dbdf5a53290d9d201b25ec18f36db76c
yxj <564197835@qq.com>
2021-08-06 16:06:22 +0800

    try to fix popover appearing in vain

--- InputSourceSwitcher/App/App.swift
+++ InputSourceSwitcher/App/App.swift
@@ -20,18 +20,20 @@ struct SourceSwitcherApp: App {
 // refer to: https://github.com/zaferarican/menubarpopoverswiftui2
 // LICENSE: https://github.com/zaferarican/menubarpopoverswiftui2/blob/master/LICENSE
 class AppDelegate: NSObject, NSApplicationDelegate {
-    @ObservedObject var MyInputSources: InputSourcesModel = InputSourcesModel() // use @StateObject will induce some problems...
+    @ObservedObject var MyInputSources = InputSourcesModel() // use @StateObject will induce some problems...
 
     var popover = NSPopover()
     var statusBarItem: NSStatusItem?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // Set the SwiftUI's ContentView to the Popover's ContentViewController
+        // Specifies the behavior of the popover.
+        // transient - The system will close the popover when the user interacts with a user interface element outside the popover.
         popover.behavior = .transient
         popover.animates = false
+        // The view controller that manages the content of the popover.
         popover.contentViewController = NSViewController()
-        // Edited
-        popover.contentViewController?.view = NSHostingView(rootView: ContentView(MyInputSources: MyInputSources))
+        popover.contentViewController?.view = NSHostingView(
+            rootView: ContentView(MyInputSources: MyInputSources))
         popover.contentViewController?.view.window?.makeKey()
         statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         statusBarItem?.button?.image = NSImage(
@@ -39,6 +41,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             accessibilityDescription: "keyboard")?
             .withSymbolConfiguration(NSImage.SymbolConfiguration(textStyle: .body, scale: .large))
         statusBarItem?.button?.action = #selector(AppDelegate.togglePopover(_:))
+
+        // When starting the app, open show the popover. If not do like this,  open the app and shortcuts will not work.
+        print(popover.isShown) // false
+        // FIXME: It's a tricky workaround. I tried lots of methods, but the window not appears when start...
+        if let button = statusBarItem?.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+        print(popover.isShown) // true (but soon it changes to false)
     }
 
     @objc func showPopover(_ sender: AnyObject?) {
@@ -52,10 +66,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     @objc func togglePopover(_ sender: AnyObject?) {
+        print("[Popover before toggling] popover.isShown: \(popover.isShown)")
         if popover.isShown {
             closePopover(sender)
         } else {
             showPopover(sender)
         }
+        print("[Popover after toggling] popover.isShown: \(popover.isShown)")
     }
 }
